8b617f2a68ac6a53e3239dff4053832e7401f3bb
katya <kotenokches@rambler.ru>
2022-04-14 15:44:52 +0500

    Первый коммит

---
+++ src/main/java/sky/pro/env_home_work_11/EnvHomeWork11Application.java
@@ -0,0 +1,13 @@
+package sky.pro.env_home_work_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvHomeWork11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvHomeWork11Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/env_home_work_11/EnvHomeWork11ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.env_home_work_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvHomeWork11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
